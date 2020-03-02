2e8ee6e461cc8205f58223f55cf4e7ef59529805
Rudi Farkas <rudi.farkas@gmail.com>
2020-03-02 21:24:54 +0100

    Initial Commit

---
+++ QRIMAGE2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRIMAGE2
+//
+//  Created by Rudolf Farkas on 02.03.20.
+//  Copyright © 2020 Rudolf Farkas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
